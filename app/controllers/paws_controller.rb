012ff0b5dc35804c55f2ae470062c8bb2e69f4f9
Kyle Brown <kylealbrown@gmail.com>
2014-11-19 17:34:57 -0500

    First commit

---
+++ app/controllers/paws_controller.rb
@@ -0,0 +1,6 @@
+class PawsController < ApplicationController
+  def index
+    response = HTTParty.get('http://www.petango.com/webservices/adoptablesearch/wsAdoptableAnimals.aspx?species=Dog&sex=A&agegroup=All&location=&site=&onhold=N&orderby=Name&colnum=2&css=http://www.petango.com/WebServices/adoptablesearch/css/styles.css&authkey=6zl6u381k1164ci4pr60263j64az7071209ra01r0kfvgl920n&recAmount=100&detailsInPopup=Yes&featuredPet=Include&stageID=')
+    @profile = Nokogiri::HTML(response.body)
+  end
+end
