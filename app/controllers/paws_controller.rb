afec53835702150bae7c11fbda7ce3dbee272a9b
Kyle Brown <kylealbrown@gmail.com>
2014-11-21 14:46:50 -0500

    beta

--- app/controllers/paws_controller.rb
+++ app/controllers/paws_controller.rb
@@ -3,4 +3,21 @@ class PawsController < ApplicationController
     response = HTTParty.get('http://www.petango.com/webservices/adoptablesearch/wsAdoptableAnimals.aspx?species=Dog&sex=A&agegroup=All&location=&site=&onhold=N&orderby=Name&colnum=2&css=http://www.petango.com/WebServices/adoptablesearch/css/styles.css&authkey=6zl6u381k1164ci4pr60263j64az7071209ra01r0kfvgl920n&recAmount=100&detailsInPopup=Yes&featuredPet=Include&stageID=')
     @profile = Nokogiri::HTML(response.body)
   end
+
+  def dog
+    @animal_id = params[:id]
+    api = "http://www.petango.com/webservices/adoptablesearch/wsAdoptableAnimalDetails.aspx?id=#{@animal_id}"
+    response = HTTParty.get(api)
+    @profile = Nokogiri::HTML(response.body)
+
+  end
+
+
+
+
+
+
+
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :pets
 
-  get 'paws/index'
+  root 'paws#index'
+  get 'dog' => 'paws#dog'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
